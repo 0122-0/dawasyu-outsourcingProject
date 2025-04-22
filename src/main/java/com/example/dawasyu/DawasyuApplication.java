766c43ee19591253f6945b4bad1e93718a813fb7
0122-0 <flowercats95@gmail.com>
2025-04-22 12:14:21 +0900

    25/04/22 프로젝트 최초 생성

---
+++ src/main/java/com/example/dawasyu/DawasyuApplication.java
@@ -0,0 +1,13 @@
+package com.example.dawasyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawasyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawasyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dawasyu/DawasyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dawasyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawasyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

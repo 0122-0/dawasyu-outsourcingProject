28efc6bed06152d3250aafef90cb6975de7a71bb
pleasebelieveme <cain1227@naver.com>
2025-04-23 19:20:49 +0900

    feat: 글로벌핸들러 에러코드 추가

---
+++ src/main/java/com/example/dawasyu/common/error/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.dawasyu.common.error;
+
+public class UserNotFoundException extends CustomException {
+
+    public UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+
+    public UserNotFoundException(String message) {
+        super(ErrorCode.USER_NOT_FOUND, message);
+    }
+}

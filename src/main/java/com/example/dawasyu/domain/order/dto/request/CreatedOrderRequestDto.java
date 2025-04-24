fc72177e0ba287a8562f60dee5acd89bcd5eb344
0122-0 <flowercats95@gmail.com>
2025-04-24 17:39:50 +0900

    feat:테스트용 수정

--- src/main/java/com/example/dawasyu/domain/order/dto/request/CreatedOrderRequestDto.java
+++ src/main/java/com/example/dawasyu/domain/order/dto/request/CreatedOrderRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.dawasyu.domain.order.dto.request;
 
 import com.example.dawasyu.domain.orderMenu.entity.OrderMenu;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 import java.util.List;
--- src/main/java/com/example/dawasyu/domain/order/service/OrderService.java
+++ src/main/java/com/example/dawasyu/domain/order/service/OrderService.java
@@ -3,8 +3,9 @@ package com.example.dawasyu.domain.order.service;
 
 import com.example.dawasyu.domain.order.dto.request.CreatedOrderRequestDto;
 import com.example.dawasyu.domain.order.dto.response.CreatedOrderResponseDto;
-import com.example.dawasyu.domain.order.dto.response.OrderResponseDto;
+
 import com.example.dawasyu.domain.order.entity.Order;
+import com.example.dawasyu.domain.order.entity.OrderStatus;
 import com.example.dawasyu.domain.orderMenu.entity.OrderMenu;
 import com.example.dawasyu.domain.user.entity.User;
 import com.example.dawasyu.repository.OrderRepository;
@@ -12,12 +13,12 @@ import com.example.dawasyu.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
+
 import java.util.Random;
 
 @Service
@@ -32,19 +33,21 @@ public class OrderService {
     @Transactional
     public CreatedOrderResponseDto createOrder (CreatedOrderRequestDto requestDto, User loginUser) {
 
-        Order order = new Order(requestDto.getOrderMenus(), loginUser);
-
         Long totalPrice = requestDto.getOrderMenus().stream()
                 .mapToLong(orderMenu -> orderMenu.getMenu().getPrice() * orderMenu.getQuantity())
                 .sum();
+
+        Order order = new Order(totalPrice, generateOrderNumber(), loginUser);
+        //gpt의 도움을 받아서 잘모름
+
         Order createdOrder = orderRepository.save(order);
 
-        // 오더 아이디랑 메뉴 아이디를 오더메뉴 객체에 넣기
-        for (OrderMenu orderMenu : createdOrder.getOrderMenus()) {
+//        // 오더 아이디랑 메뉴 아이디를 오더메뉴 객체에 넣기
+//        for (OrderMenu orderMenu : requestDto.getOrderMenus()) {
+//
+//        }
 
-        }
-
-        return new CreatedOrderResponseDto(generateOrderNumber(), createdOrder.getOrderMenus(), totalPrice , createdOrder.getCreatedAt());
+        return new CreatedOrderResponseDto(createdOrder.getOrderNumber(), createdOrder.getTotalPrice(), createdOrder.getCreatedAt());
     }
 
     public static String generateOrderNumber() {

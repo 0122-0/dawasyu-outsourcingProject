c334f239db40c79322484906b6d241a7bbb5bcba
DESKTOP-M7S61C8\박수박 <pospara2@gmail.com>
2025-04-28 19:08:00 +0900

    ãFEAT : STORE VAILATION, CATEGORY GETALL

--- src/main/java/com/example/dawasyu/domain/category/service/CategoryService.java
+++ src/main/java/com/example/dawasyu/domain/category/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.example.dawasyu.domain.category.service;
 
+import com.example.dawasyu.common.error.CustomException;
+import com.example.dawasyu.common.error.ErrorCode;
 import com.example.dawasyu.domain.category.dto.request.CategoryCreateRequestDTO;
 import com.example.dawasyu.domain.category.dto.response.CategoryResponseDTO;
 import com.example.dawasyu.domain.category.entity.Category;
@@ -7,6 +9,9 @@ import com.example.dawasyu.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +22,10 @@ public class CategoryService {
     @Transactional
     public CategoryResponseDTO createCategory(CategoryCreateRequestDTO reqDTO) {
 
+        if (categoryRepository.existsByName(reqDTO.getName())) {
+            throw new CustomException(ErrorCode.DUPLICATE_CATEGORY_NAME);
+        }
+
         Category categoryToSave = reqDTO.toEntity();
 
         return CategoryResponseDTO.toDto(categoryRepository.save(categoryToSave));
@@ -25,6 +34,15 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public Category getCategoryById(Long id) {
         return categoryRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 카테고리가 존재하지 않습니다. id=" + id));
+                .orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
+    public List<CategoryResponseDTO> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+
+        return categories.stream()
+                .map(CategoryResponseDTO::toDto)
+                .collect(Collectors.toList());
     }
 }
